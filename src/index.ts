4c74e48fa5a0e2533bbb926580907e919e9934a0
Oliver Saxon <oliverrsaxon@gmail.com>
2023-11-07 13:10:01 +0000

    initial project set up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const __dirName = import.meta.dir;
+
+async function getCats() {
+    const data = Bun.file(`${__dirName}/data/cats.json`);
+    const cats = await data.json();
+    return cats;
+}
+
+const app = new Elysia()
+    .get("/", () => "Hello Elysia")
+    .get("/api", () => {
+        console.log(Bun.env.PORT, "<--- port num");
+        return {
+            message: "api route",
+            other: ["param1", "param2"],
+            port: Bun.env.PORT || process.env.PORT || null,
+            env: Bun.env,
+        };
+    })
+    .get("/api/books/:id", ({ params: { id } }) => {
+        return { id };
+    })
+    .get("/api/cats", () => getCats())
+    .listen(process.env.PORT ?? 9090);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
